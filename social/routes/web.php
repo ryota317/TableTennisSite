5c55528e88d84b3b42006139d40a6aab0d1076d8
ryota <ryotaa7@icloud.com>
2020-07-10 15:35:29 +0900

    コード整理

--- social/routes/web.php
+++ social/routes/web.php
@@ -17,47 +17,34 @@ Route::get('test', 'UserController@index');
 Route::post('/sign_up', 'UserController@signUp');
 Route::post('/sign_in', 'UserController@signIn');
 
-
 Route::get('/wel', function () {
     return view('welcome');
 });
-
 
 Route::get('/', function () {
     return view('index');
 });
 
-
-
-
-
-
 Route::get('/practice_info_list', 'PracticeController@practice_info_list');
-
-
 
 Route::get('/sign_in_page', function () {
     return view('sign_in_page');
 });
+
 Route::get('/sign_up_page', function () {
     return view('sign_up_page');
 });
 
-
-
 Route::get('/profile', 'UserController@profile');
-Route::get('/create_team', 'UserController@create_team');
-Route::get('/practice_partner', 'UserController@practice_partner');
 
+Route::get('/create_team', 'UserController@create_team');
+
+Route::get('/practice_partner', 'UserController@practice_partner');
 
 Route::get('/team_search', 'TeamController@team_search');
 
 Route::get('/sign_out', 'UserController@sign_out');
 
-
 Route::post('/practice_partner_post', 'PracticeController@practice_partner_post');
-
-
-
 
 Route::post('/create_team', 'TeamController@create_team');
